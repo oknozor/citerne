47f8e0898de5a92cc140c5d6ed97b6331d001626
Paul Delafosse <paul.delafosse@protonmail.com>
2022-09-22 12:42:49 +0200

    feat: add feature conflict

--- citerne-derive/src/lib.rs
+++ citerne-derive/src/lib.rs
@@ -9,16 +9,35 @@ mod attributes;
 #[proc_macro_attribute]
 pub fn database_container(attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut function = parse_macro_input!(item as syn::ItemFn);
-    let attributes = parse_macro_input!(attr as DieselTankerAttributes);
 
+    let attributes = parse_macro_input!(attr as DieselTankerAttributes);
+    #[cfg(feature = "postgres")]
+    let mut init_container_statements = generate_postgres_container_init(attributes);
+
+    #[cfg(feature = "mysql")]
+    let mut init_container_statements = generate_mysql_container_init(attributes);
+
+    init_container_statements.extend(function.block.stmts.clone());
+    function.block.stmts = init_container_statements;
+
+    let function = quote! {
+        #function
+    };
+
+    TokenStream::from(function)
+}
+
+#[cfg(feature = "postgres")]
+fn generate_postgres_container_init(attributes: DieselTankerAttributes) -> Vec<Stmt> {
     let mut init_container_statements: Vec<Stmt> = parse_quote! {
             let docker = testcontainers::clients::Cli::default();
+            println!("Starting postgresql container");
             let postgres: testcontainers::Container<testcontainers::images::postgres::Postgres> = docker.run(testcontainers::images::postgres::Postgres::default());
             let addr = format!(
                 "postgresql://postgres@127.0.0.1:{}/postgres",
                 postgres.get_host_port_ipv4(5432)
             );
-
+            println!("Container started, trying to establish postgresql connection on port 5432");
             let mut conn = {
                 use diesel::Connection;
                 diesel::PgConnection::establish(&addr)?
@@ -31,14 +50,16 @@ pub fn database_container(attr: TokenStream, item: TokenStream) -> TokenStream {
             let crate_path = std::env::var("CARGO_MANIFEST_DIR").map(|dir|std::path::PathBuf::from(dir))?;
             let migration_path = crate_path.join(#migration);
             if !migration_path.is_dir() {
-                let sql = std::fs::read_to_string(migration_path)?;
+                let sql = std::fs::read_to_string(&migration_path)?;
                 {
                     use diesel::connection::SimpleConnection;
+                    println!("Running single migration {}", migration_path.file_name().unwrap().to_string_lossy());
                     conn.batch_execute(&sql)?;
                 }
             } else {
-                let migrations = diesel_migrations::FileBasedMigrations::from_path(migration_path)?.migrations()?;
+                let migrations = diesel_migrations::FileBasedMigrations::from_path(&migration_path)?.migrations()?;
                 for migration in migrations {
+                    println!("Running migrations {}", migration.name());
                     migration.run(&mut conn)?;
                 }
             }
@@ -46,13 +67,49 @@ pub fn database_container(attr: TokenStream, item: TokenStream) -> TokenStream {
 
         init_container_statements.extend::<Vec<Stmt>>(run_migrations);
     }
+    init_container_statements
+}
 
-    init_container_statements.extend(function.block.stmts.clone());
-    function.block.stmts = init_container_statements;
+// TODO
+#[cfg(feature = "mysql")]
+fn generate_mysql_container_init(attributes: DieselTankerAttributes) -> Vec<Stmt> {
+    let mut init_container_statements: Vec<Stmt> = parse_quote! {
+            let docker = testcontainers::clients::Cli::default();
+            println!("Starting postgresql container");
+            let postgres: testcontainers::Container<testcontainers::images::postgres::Postgres> = docker.run(testcontainers::images::postgres::Postgres::default());
+            let addr = format!(
+                "postgresql://postgres@127.0.0.1:{}/postgres",
+                postgres.get_host_port_ipv4(5432)
+            );
+            println!("Container started, trying to establish postgresql connection on port 5432");
+            let mut conn = {
+                use diesel::Connection;
+                diesel::PgConnection::establish(&addr)?
+            };
+        };
 
-    let function = quote! {
-        #function
-    };
 
-    TokenStream::from(function)
+    for migration in attributes.migrations {
+        let run_migrations = parse_quote! {
+            let crate_path = std::env::var("CARGO_MANIFEST_DIR").map(|dir|std::path::PathBuf::from(dir))?;
+            let migration_path = crate_path.join(#migration);
+            if !migration_path.is_dir() {
+                let sql = std::fs::read_to_string(&migration_path)?;
+                {
+                    use diesel::connection::SimpleConnection;
+                    println!("Running single migration {}", migration_path.file_name().unwrap().to_string_lossy());
+                    conn.batch_execute(&sql)?;
+                }
+            } else {
+                let migrations = diesel_migrations::FileBasedMigrations::from_path(&migration_path)?.migrations()?;
+                for migration in migrations {
+                    println!("Running migrations {}", migration.name());
+                    migration.run(&mut conn)?;
+                }
+            }
+        };
+
+        init_container_statements.extend::<Vec<Stmt>>(run_migrations);
+    }
+    init_container_statements
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ use std::error::Error;
 use std::path::PathBuf;
 use diesel::migration::{Migration, MigrationSource};
 use diesel_migrations::{embed_migrations, EmbeddedMigration, FileBasedMigrations, MigrationHarness};
+use testcontainers::clients::Cli;
 use gazole_test_derive::database_container;
 
+#[cfg(all(feature = "mysql", feature = "postgres"))]
+compile_error!("Feature 'mysql' and 'postgres' cannot be enabled together");
+
 type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
-
-
 
 
 #[test]
